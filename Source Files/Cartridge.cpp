547f2497f1514e4a77b14a827fc6d469053aba2c
Scott Duncan <scott.duncan.work@gmail.com>
2021-07-03 22:25:20 +0100

    Initialised functions from header

--- Source Files/Cartridge.cpp
+++ Source Files/Cartridge.cpp
@@ -1,1 +1,21 @@
 #include "Cartridge.h"
+
+uint8_t cpuRead(uint16_t addr, bool rdonly = false)
+{
+
+}
+
+void cpuWrite(uint16_t addr, uint8_t data)
+{
+
+}
+
+uint8_t ppuRead(uint16_t addr, bool rdonly = false)
+{
+
+}
+
+void ppuWrite(uint16_t addr, uint8_t data)
+{
+
+}
