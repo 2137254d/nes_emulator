ca795627cde90737544ac63b9c2540c604004105
Scott Duncan <scott.duncan.work@gmail.com>
2021-07-04 19:28:28 +0100

    Added functionaliy to PPU

--- Header Files/olc2C02.h
+++ Header Files/olc2C02.h
@@ -1,11 +1,18 @@
 #pragma once
 #include <cstdint>
+#include <memory>
+#include "Cartridge.h"
 
 class olc2C02
 {
 public:
     olc2C02();
     ~olc2C02();
+
+private:
+    uint8_t     tblName[2][1024]; // VRAM Name table
+    uint8_t     tblPalette[32]; // RAM that stores pallet information
+    uint8_t     tblPattern[2][4096];
 
 public:
     // Communications with Main Bus
@@ -16,4 +23,12 @@ public:
     uint8_t  ppuRead(uint16_t addr, bool rdonly = false);
     void     ppuWrite(uint16_t addr, uint8_t data);
 
+public:
+    // Interface
+    void ConnectCartridge(const std::shared_ptr<Cartridge>& cartridge);
+    void clock();
+
+private:
+    // The Cartridge
+    std::shared_ptr<Cartridge> cart;
 };
