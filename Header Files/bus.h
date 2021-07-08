9675cc8baad70053c47614805de52a07003ba691
Scott Duncan <scott.duncan.work@gmail.com>
2021-07-08 20:18:33 +0100

    Finished Bus class for now

--- Header Files/bus.h
+++ Header Files/bus.h
@@ -31,10 +31,11 @@ public: // Bus read and write
 private:
 	// A count of how many clocks have passed 
 	uint32_t nSystemClockCounter = 0;
-	// internal cache of controller state
-	uint8_t contoroller_state[2];
+	// Internal cache of controller state
+	uint8_t controller_state[2];
 
 public: // System Interface
+	// Connects a cartridge object to the internal buses
 	void insertCartridge(const std::shared_ptr<Cartridge>& cartridge);
 	void reset();
 	void clock();
--- Source Files/Bus.cpp
+++ Source Files/Bus.cpp
@@ -35,9 +35,10 @@ void Bus::cpuWrite(uint16_t addr, uint8_t data)
 
 uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly)
 { 
-	if (cart->cpuRead(addr, bReadOnly))
+	uint8_t data = 0x00;
+	if (cart->cpuRead(addr, data))
 	{
-		
+		// Cartridge address range
 	}
 	else if (addr >= 0x0000 && addr <= 0x1FFF) {
 		return cpuRam[addr & 0x07FF];
@@ -46,22 +47,43 @@ uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly)
 	{
 		return ppu.cpuRead(addr & 0x0007, bReadOnly);
 	}
-	return 0x00;
+	else if ( addr >= 0x4016 && addr <= 0x4017)
+	{
+		data = (controller_state[addr & 0x0001] & 0x80) > 0;
+		controller_state[addr & 0x0001] <<= 1;
+	}
+	return data;
 }
 
 void Bus::insertCartridge(const std::shared_ptr<Cartridge>& cartridge)
 {
+	// Connects cartridge to both Main Bus and CPU Bus
 	this->cart = cartridge;
 	ppu.ConnectCartridge(cartridge);
 }
 
 void Bus::reset()
 {
+	cart->reset();
 	cpu.reset();
+	ppu.reset();
 	nSystemClockCounter = 0;
 }
 
 void Bus::clock()
 {
+	ppu.clock();
+	if (nSystemClockCounter % 3 == 0)
+	{
+		cpu.clock();
+	}
 
+	if (ppu.nmi)
+	{
+		ppu.nmi = false;
+		cpu.nmi();
+	}
+
+
+	nSystemClockCounter++;
 }
