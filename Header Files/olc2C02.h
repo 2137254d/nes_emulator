7f38b1be48950645c35bab89d7f3c4780e8e3490
Scott Duncan <scott.duncan.work@gmail.com>
2021-07-11 16:58:38 +0100

    Added background rendering variables

--- Header Files/olc2C02.h
+++ Header Files/olc2C02.h
@@ -103,5 +103,44 @@ private:
     uint8_t address_latch = 0x00;
     uint8_t ppu_data_buffer = 0x00;
     uint16_t ppu_address = 0x0000;
-    
+
+public: 
+    union loopy_register
+    {
+        struct 
+        {
+            
+            uint16_t course_x : 5;
+            uint16_t course_y : 5;
+            uint16_t nametable_x : 1;
+            uint16_t nametable_y : 1;
+            uint16_t fine_y : 3;
+            uint16_t unused : 1;
+        };
+
+        uint16_t reg = 0x0000;
+    }; 
+
+    loopy_register vram_addr;
+    loopy_register tram_addr;
+
+    uint8_t fine_x = 0x00; // Pixel offset horizontally
+
+    // Internal communications
+    uint8_t address_latch = 0x00;
+    uint8_t ppu_data_buffer = 0x00;
+
+    // Pixel "dot" position information
+    int16_t scanline = 0;
+    int16_t cycle = 0;
+
+    // Backgorund rendering
+    uint8_t bg_next_title_id = 0x00;
+    uint8_t bg_next_title_attrib = 0x00;
+    uint8_t bg_next_title_lsb = 0x00;
+    uint8_t bg_next_title_msb = 0x00;
+    uint16_t bg_shifter_pattern_lo = 0x0000;
+    uint16_t bg_shifter_pattern_hi = 0x0000;
+    uint16_t bg_shifter_attrib_lo = 0x0000;
+    uint16_t bg_shifter_attrib_hi = 0x0000;
 };
