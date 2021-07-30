a5c18dc0ea0dab9a2e8c2438ef43880241c202f1
Scott Duncan <scott.duncan.work@gmail.com>
2021-07-30 07:54:25 +0100

    Added mapper 001 functions to header

--- Header Files/Mapper_001.h
+++ Header Files/Mapper_001.h
@@ -8,4 +8,14 @@ class Mapper_001 : public Mapper
 public:
         Mapper_001(uint8_t prgBanks, uint8_t chrBanks);
         ~Mapper_001();
+
+        bool cpuMapRead(uint16_t addr, uint32_t &mapped_addr, uint8_t data) override;
+        bool cpuMapWrite(uint16_t addr, uint32_t &mapped_addr, uint8_t data =0) override;
+        bool ppuMapRead(uint16_t addr, uint32_t &mapped_addr) override;
+        bool ppuMapWrite(uint16_t addr, uint32_t &mapped_addr) override;
+        void reset() override;
+        MIRROR mirror();
+
+private: 
+        
 };
